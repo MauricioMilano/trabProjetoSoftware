dc101ca21ada32d66f88c774d5e4795e57450c95
Mauricio Milano <mauricio.milano@.ibm.com>
2020-10-10 21:28:19 -0300

    refazendo td

---
+++ src/main/java/com/uff/trab/projetodeSoftware/Application.java
@@ -0,0 +1,13 @@
+package com.uff.trab.projetodeSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uff/trab/projetodeSoftware/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uff.trab.projetodeSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
